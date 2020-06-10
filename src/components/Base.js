9c89ea671f2bdcbb9e32dc34c045d908990c7bdf
dpak1999 <deepakkumardash313@gmail.com>
2020-06-10 13:53:25 +0530

    adding variants

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,15 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", delay: 0.5 },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: { type: "spring", stiffness: 120 },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 1 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -21,7 +43,7 @@ const Base = ({ addBase, pizza }) => {
               key={base}
               onClick={() => addBase(base)}
               whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
-              transition={{ type: "spring", stiffness: 300 }}
+              transition={{}}
             >
               <span className={spanClass}>{base}</span>
             </motion.li>
@@ -30,12 +52,7 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          className="next"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", stiffness: 120 }}
-        >
+        <motion.div className="next" variants={nextVariants}>
           <Link to="/toppings">
             <motion.button
               whileHover={{
